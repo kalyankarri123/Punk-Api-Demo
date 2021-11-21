3c1855d9a06393ddbb539b389a3cd577f3483cf4
kalyankarri123 <94766044+kalyankarri123@users.noreply.github.com>
2021-11-21 06:17:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+
+
 const endpoint = "https://api.punkapi.com/v2/beers"; 
-$.getJSON(endpoint, function (data) {  
-  console.log(data); 
+
+$.getJSON(endpoint, function (data) {
+  
+  console.log(data);
+  let allBeers = data.filter(beer => beer.abv <= 4.5 && beer.abv > 7.5 &&  beer.abv >50 );
   let weakBeers = data.filter(beer => beer.abv <= 4.5);
   let medBeers = data.filter(beer => beer.abv > 4.5 && beer.abv <= 7.5);
-  let strongBeers = data.filter(beer => beer.abv > 7.5 && beer.abv <= 50); // pass in filtered data  and class
-
+  let strongBeers = data.filter(beer => beer.abv > 7.5 && beer.abv <= 50); 
   function Display(range, percent) {
-    // build html with filted data
     let beerHtml = range.map(item => `
         <div class = 'beer-wrapper'>
         <div class = "beer ${percent}">
-          <i class="fa fa-star" aria-hidden="true"></i>
           <h3 class="beer__name">${item.name}</h3>
           <img class ="beer__img" src = "${item.image_url}">
           <h4 class ="beer__tagline">${item.tagline}</h4>
@@ -35,6 +37,7 @@ $.getJSON(endpoint, function (data) {
             `);
     $(".beers").append(beerHtml);
   } 
+    Display(allBeers, "all");
   Display(weakBeers, "weak");
   Display(medBeers, "medium");
   Display(strongBeers, "strong"); 
@@ -47,7 +50,7 @@ $(".button").on("click", function () {
     $(".bg").hide(0); 
 });
   $(".beer").css("display", "none");
-  $(".beers .medium").css("display", "block"); // filter beers using tabs
+  $(".beers .medium").css("display", "block"); 
   $(".tab__item").on("click", function () {
     $(".tab__item").removeClass("active");
     $(this).addClass("active");
@@ -58,16 +61,19 @@ $(".button").on("click", function () {
     $(".beers .strong").show();
   });
   $(".tab__item.weak").on("click", function () {
+    $(".beers .all").hide();
     $(".beers .weak").show();
     $(".beers .medium").hide();
     $(".beers .strong").hide();
   });
   $(".tab__item.medium").on("click", function () {
+    $(".beers .all").hide();
     $(".beers .medium").show();
     $(".beers .weak").hide();
     $(".beers .strong").hide();
   });
   $(".tab__item.strong").on("click", function () {
+    $(".beers .all").hide();
     $(".beers .strong").show();
     $(".beers .weak").hide();
     $(".beers .medium").hide();
